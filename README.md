a9b7e162db85445bcf4e72ab81b8766bb30e0659
René KAKPO <rene@right-com.com>
2023-08-14 12:28:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+**GitHub Repository README**
+
+# TextSnap - Android Text Recognition App
+
+![TextSnap Logo](app_logo.png)
+
+TextSnap is a simple and intuitive Android app designed to demonstrate the capabilities of Google Vision Text Recognition. With TextSnap, you can easily capture text from images using your device's front camera and view the recognized text in real-time on the app's main screen. It's perfect for developers who want to explore Google Vision's text recognition features or for anyone looking for a straightforward way to extract text from images.
+
+## Features
+
+- **Front Camera Text Recognition**: Utilize your device's front camera to capture images containing text. TextSnap leverages the cameraSource feature to process live images.
+
+- **Real-time Text Display**: The recognized text is instantly displayed on the app's main screen, providing a seamless and responsive user experience.
+
+- **Minimalistic UI**: TextSnap is designed with simplicity in mind. The app features a single screen, making it easy to understand and use.
+
+## Screenshots
+
+![TextSnap Screenshot](screenshot.png)
+
+## Installation
+
+1. Clone this repository to your local machine using:
+   ```
+   git clone https://github.com/Renekakpo/TextSnap.git
+   ```
+
+2. Open the project in Android Studio.
+
+3. Build and run the app on an Android emulator or a physical device.
+
+## Usage
+
+1. Launch the TextSnap app on your device.
+
+2. Allow the app to access your device's camera.
+
+3. Point the front camera at an image containing text.
+
+4. The recognized text will be displayed in real-time at the bottom of the screen.
+
+## Dependencies
+
+TextSnap uses the Google Vision API for text recognition. The necessary dependencies are already included in the project's `build.gradle` file.
+
+```groovy
+implementation 'com.google.android.gms:play-services-vision:20.1.3'
+```
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvements, feel free to open an issue or create a pull request.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+---
+
+*Disclaimer: "TextSnap" is a fictional name created for the purpose of this example.*
---
+++ app/src/main/java/com/renekakpo/ocrappdemo/MainActivity.kt
@@ -0,0 +1,156 @@
+package com.renekakpo.ocrappdemo
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.view.SurfaceHolder
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.Detector.Detections
+import com.google.android.gms.vision.text.TextBlock
+import com.google.android.gms.vision.text.TextRecognizer
+import com.renekakpo.ocrappdemo.databinding.ActivityMainBinding
+import java.io.IOException
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var mCameraSource: CameraSource
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+        private const val requestPermissionID = 101
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        startCameraSource()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        if (requestCode != requestPermissionID) {
+            Log.d(TAG, "Got unexpected permission result: $requestCode")
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            return
+        }
+
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            try {
+                if (ActivityCompat.checkSelfPermission(
+                        this@MainActivity,
+                        Manifest.permission.CAMERA
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    return
+                }
+                mCameraSource.start(binding.cameraView.holder)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun startCameraSource() {
+        try {
+
+            //Create the TextRecognizer
+            val textRecognizer = TextRecognizer.Builder(applicationContext).build()
+
+            mCameraSource = CameraSource.Builder(applicationContext, textRecognizer)
+                .setFacing(CameraSource.CAMERA_FACING_BACK)
+                .setRequestedPreviewSize(1280, 1024)
+                .setAutoFocusEnabled(true)
+                .setRequestedFps(2.0f)
+                .build()
+
+            if (!textRecognizer.isOperational) {
+                Log.w(TAG, "Detector dependencies not loaded yet")
+            } else {
+                //Initialize cameraSource to use high resolution and set Autofocus on.
+                mCameraSource = CameraSource.Builder(applicationContext, textRecognizer)
+                    .setFacing(CameraSource.CAMERA_FACING_FRONT)
+                    .setRequestedPreviewSize(1280, 1024)
+                    .setAutoFocusEnabled(true)
+                    .setRequestedFps(2.0f)
+                    .build()
+
+                /**
+                 * Add call back to SurfaceView and check if camera permission is granted.
+                 * If permission is granted we can start our cameraSource and pass it to surfaceView
+                 */
+                binding.cameraView.holder.addCallback(object : SurfaceHolder.Callback {
+                    override fun surfaceCreated(holder: SurfaceHolder) {
+                        try {
+                            if (ActivityCompat.checkSelfPermission(
+                                    applicationContext,
+                                    Manifest.permission.CAMERA
+                                ) != PackageManager.PERMISSION_GRANTED
+                            ) {
+                                ActivityCompat.requestPermissions(
+                                    this@MainActivity, arrayOf(Manifest.permission.CAMERA),
+                                    requestPermissionID
+                                )
+                                return
+                            }
+                            mCameraSource.start(binding.cameraView.holder)
+                        } catch (e: IOException) {
+                            e.printStackTrace()
+                        }
+                    }
+
+                    override fun surfaceChanged(
+                        holder: SurfaceHolder,
+                        format: Int,
+                        width: Int,
+                        height: Int
+                    ) {
+                    }
+
+                    /**
+                     * Release resources for cameraSource
+                     */
+                    override fun surfaceDestroyed(holder: SurfaceHolder) {
+                        mCameraSource.stop()
+                    }
+                })
+
+                //Set the TextRecognizer's Processor.
+                textRecognizer.setProcessor(object : Detector.Processor<TextBlock> {
+                    override fun release() {}
+
+                    /**
+                     * Detect all the text from camera using TextBlock and the values into a stringBuilder
+                     * which will then be set to the textView.
+                     */
+                    override fun receiveDetections(detections: Detections<TextBlock>) {
+                        val items = detections.detectedItems
+                        if (items.size() != 0) {
+                            binding.textView.post {
+                                val stringBuilder = StringBuilder()
+                                for (i in 0 until items.size()) {
+                                    val item = items.valueAt(i)
+                                    stringBuilder.append(item.value)
+                                    stringBuilder.append("\n")
+                                }
+                                binding.textView.text = stringBuilder.toString()
+                            }
+                        }
+                    }
+                })
+            }
+        } catch (e: Exception) {
+            Log.e("startCameraSource", "${e.message}")
+        }
+    }
+}
